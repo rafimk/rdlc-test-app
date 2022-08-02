6ebec44766e4924e44713a0a0074cee7a716093d
Muhammed Rafi <m.rafi@afad.gov.ae>
2022-08-02 10:54:56 +0400

    Added download pdf file

--- Controllers/PdfCreatorController.cs
+++ Controllers/PdfCreatorController.cs
@@ -43,4 +43,34 @@ public class PdfCreatorController : ControllerBase
         _converter.Convert(pdf);
         return Ok("Successfully created PDF document.");
     }
+
+    [HttpGet("/download")]
+    public ActionResult Get(string reportName)
+    {
+         var globalSettings = new GlobalSettings
+        {
+            ColorMode = ColorMode.Color,
+            Orientation = Orientation.Portrait,
+            PaperSize = PaperKind.A4,
+            Margins = new MarginSettings { Top = 10 },
+            DocumentTitle = "PDF Report",
+        };
+
+        var objectSettings = new ObjectSettings
+        {
+            PagesCount = true,
+            HtmlContent = TemplateGenerator.GetHTMLString(),
+            WebSettings = { DefaultEncoding = "utf-8", UserStyleSheet =  Path.Combine(Directory.GetCurrentDirectory(), "assets", "styles.css") },
+            HeaderSettings = { FontName = "Arial", FontSize = 9, Right = "Page [page] of [toPage]", Line = true },
+            FooterSettings = { FontName = "Arial", FontSize = 9, Line = true, Center = "Report Footer" }
+        };
+        var pdf = new HtmlToPdfDocument()
+        {
+            GlobalSettings = globalSettings,
+            Objects = { objectSettings }
+        };
+        
+        var returnString = _converter.Convert(pdf);;
+        return File(returnString, System.Net.Mime.MediaTypeNames.Application.Octet, reportName + ".pdf");
+    }
 }
